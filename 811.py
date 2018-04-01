ca98b2819885629ccf364e4eb026db7c6f283369
KIRTISHD <aniket.dhande@gmail.com>
2018-04-01 09:34:23 +0530

    I've lost count now

---
+++ 811.py
@@ -0,0 +1,49 @@
+#Subdomain visit count
+class Solution:
+    dic = dict()
+
+
+    def partit(self, S, part):
+        return S.partition(part)
+
+
+    def add(self, S, num):
+        if S in Solution.dic.keys():
+            Solution.dic[S] = Solution.dic[S] + int(num)
+        else:
+            Solution.dic[S] = int(num)
+
+        stri = Solution.partit(S, '.')
+        if stri[0] in Solution.dic.keys():
+            Solution.dic[S[2]] = Solution.dic[S[2]] + int(num)
+        if stri[2].__contains__('.'):
+            Solution.add(stri[2], num)
+        else:
+            if stri[2] in Solution.dic.keys():
+                Solution.dic[stri[2]] = Solution.dic[stri[2]] + int(num)
+            else:
+                Solution.dic[stri[2]] = int(num)
+        return
+
+
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        self.dic.clear()
+        for x in range(len(cpdomains)):
+            S = Solution.partit(cpdomains[x], ' ')
+            Solution.add(S[2], S[0])
+
+        final_list = list()
+        for x, y in Solution.dic.items():
+            sss = str(y) + " " + str(x)
+            final_list.append(sss)
+        return final_list
+
+
+if __name__=="__main__":
+    cases = ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+    final_list=Solution.subdomainVisits(cases)
+    print (final_list)
---
+++ sum_of_two_numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+
+
+    def twoSum( nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        sorted_nums = nums.sort()
+
+
+
+    if __name__ == "__main__":
+        nums = [2,7,11,15]
+        target = 9
+        a = twoSum(nums,target)
+        print (a)
